786db6444844e6c446248d396dfc47d62445a8f0
Kirsten <git@science-based.org>
2015-07-29 21:26:43 +0200

    added Button to GridPane

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,9 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void buttonClicked(ActionEvent actionEvent) {
+        System.out.println("This button as clicked!");
+    }
 }
